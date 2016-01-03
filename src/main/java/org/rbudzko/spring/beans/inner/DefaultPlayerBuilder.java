6a8fca74ae3ef5a66fbc52f327d664069830b38a
Robert Budźko <robert.krzysztof.budzko@gmail.com>
2016-01-03 18:34:45 +0100

    Missing final in method's signature.

--- src/main/java/org/rbudzko/spring/beans/inner/DefaultPlayerBuilder.java
+++ src/main/java/org/rbudzko/spring/beans/inner/DefaultPlayerBuilder.java
@@ -8,7 +8,7 @@ public class DefaultPlayerBuilder {
      * This method is added to increase complexity, because often it is not possible to create beans by constructors.
      * For example eternal libraries force us to use builders or other creations not aligned with Spring's ways.
      */
-    public static DefaultPlayer forName(String name) {
+    public static DefaultPlayer forName(final String name) {
         return new DefaultPlayer(name);
     }
 }
